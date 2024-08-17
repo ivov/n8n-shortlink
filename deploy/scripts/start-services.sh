7c1639d3c8f18f521a67b63c0170cae6ac6e6ec4
Iván Ovejero <ivov.src@gmail.com>
2024-08-17 20:44:48 +0200

    Clean up docker-compose commands

--- deploy/scripts/start-services.sh
+++ deploy/scripts/start-services.sh
@@ -11,10 +11,10 @@ RAW_REPO_ROOT="https://raw.githubusercontent.com/$GITHUB_USER/$GITHUB_REPO"
 curl -o deploy/docker-compose.monitoring.yml $RAW_REPO_ROOT/main/deploy/docker-compose.monitoring.yml
 curl -o deploy/docker-compose.yml $RAW_REPO_ROOT/main/deploy/docker-compose.yml
 
-COMPOSE_PROJECT_NAME=n8n_shortlink docker compose --file deploy/docker-compose.monitoring.yml pull
-COMPOSE_PROJECT_NAME=n8n_shortlink docker compose --file deploy/docker-compose.yml pull
+docker-compose --file deploy/docker-compose.monitoring.yml pull
+docker-compose --file deploy/docker-compose.yml pull
 
-COMPOSE_PROJECT_NAME=n8n_shortlink docker compose --file deploy/docker-compose.monitoring.yml up -d
-COMPOSE_PROJECT_NAME=n8n_shortlink docker compose --file deploy/docker-compose.yml --profile production up -d
+COMPOSE_PROJECT_NAME=n8n_shortlink docker-compose --file deploy/docker-compose.monitoring.yml up -d
+COMPOSE_PROJECT_NAME=n8n_shortlink docker-compose --file deploy/docker-compose.yml --profile production up -d
 
 # Adding `COMPOSE_PROJECT_NAME=n8n_shortlink` prevents Docker from prefixing volume names with this dir name.
